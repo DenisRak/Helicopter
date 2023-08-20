f39e2693f837ebb24e32ceeba99665b2a1b7da2f
DenisRak <116069802+DenisRak@users.noreply.github.com>
2023-08-20 21:38:20 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,44 @@
+#🔥,🌳,🚁,🟩,🟦,💛,☁️,⚡,🪣,🏪,🆘,❌
+from pynput import keyboard
+from map import Map
+import time
+import os
+from helicopter import Helicopter as Helico
+
+TICK_SLEEP = 0.05
+TREE_UPDATE = 50
+FIRE_UPDATE = 100
+MAP_W, MAP_H = 30, 10
+
+field = Map(MAP_W, MAP_H)
+
+helico = Helico(MAP_W, MAP_H)
+
+MOVES = {'w': (-1,0), 'd': (0,1), 's': (1,0), 'a': (0,-1)}
+def process_key(key):
+    global helico
+    c = key.char.lower()
+    if c in MOVES.keys():
+        dx, dy = MOVES[c][0], MOVES[c][1]
+        helico.move(dx, dy)
+        
+listener = keyboard.Listener(        
+    on_press=process_key,
+    on_release=None,)
+listener.start()
+        
+
+tick = 1
+
+while True:
+    os.system("cls")
+    print("TICK", tick)
+    field.process_helicopter(helico)
+    helico.print_stats()
+    field.print_map(helico)
+    tick += 1
+    time.sleep(TICK_SLEEP)
+    if (tick % TREE_UPDATE == 0):
+        field.generate_tree()
+    if (tick % FIRE_UPDATE == 0):
+        field.update_fires()    
